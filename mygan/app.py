d6f2ee55f3270c649c68a751a326d6541f15fbd6
Ryosuke Hayashi <hayashi@hayashikun.com>
2020-12-06 15:16:10 +0900

    Add gan models

--- mygan/app.py
+++ mygan/app.py
@@ -2,7 +2,7 @@ import os
 
 import fire
 
-from mygan import TmpFilePath, capture, s3
+from mygan import TmpFilePath, capture, s3, gan
 
 
 def camera():
@@ -11,6 +11,11 @@ def camera():
 
 def sync_face():
     s3.sync(os.path.join(TmpFilePath, "face"), "mygan/face/")
+
+
+def train(n_epochs=50):
+    gan.train(n_epochs)
+
 
 if __name__ == "__main__":
     fire.Fire()
---
+++ mygan/discriminator.py
@@ -0,0 +1,24 @@
+from torch import nn
+
+
+class Discriminator(nn.Module):
+    def __init__(self, image_size, n_channel):
+        super(Discriminator, self).__init__()
+        self.main = nn.Sequential(
+            nn.Conv2d(n_channel, image_size, 4, 2, 1, bias=False),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.Conv2d(image_size, image_size * 2, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(image_size * 2),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.Conv2d(image_size * 2, image_size * 4, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(image_size * 4),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.Conv2d(image_size * 4, image_size * 8, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(image_size * 8),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.Conv2d(image_size * 8, 1, 4, 1, 0, bias=False),
+            nn.Sigmoid()
+        )
+
+    def forward(self, input_vec):
+        return self.main(input_vec)
