371ccb82ceec0d9d6769b4d92df368a8c138863b
Ryosuke Hayashi <hayashi@hayashikun.com>
2020-12-06 15:15:05 +0900

    Add s3 util

--- mygan/app.py
+++ mygan/app.py
@@ -1,10 +1,16 @@
+import os
+
 import fire
-from mygan import capture
+
+from mygan import TmpFilePath, capture, s3
 
 
 def camera():
     capture.capture()
 
+
+def sync_face():
+    s3.sync(os.path.join(TmpFilePath, "face"), "mygan/face/")
 
 if __name__ == "__main__":
     fire.Fire()
