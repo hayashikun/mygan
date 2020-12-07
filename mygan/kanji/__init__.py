0d6d772918ea9292322f114e79a5605594d38603
Ryosuke Hayashi <hayashi@hayashikun.com>
2020-12-07 12:33:25 +0900

    Find font

--- mygan/kanji/__init__.py
+++ mygan/kanji/__init__.py
@@ -1,5 +1,6 @@
 from PIL import Image, ImageDraw, ImageFont
 import os
+import platform
 from mygan import PackageRoot, TmpFilePath
 
 KANJI_IMG_PATH = os.path.join(TmpFilePath, "kanji")
@@ -8,12 +9,20 @@ if not os.path.exists(KANJI_IMG_PATH):
     os.makedirs(KANJI_IMG_PATH)
 
 
+def font_location():
+    pf = platform.system()
+    if pf == 'Darwin':
+        return "/System/Library/Fonts/ヒラギノ明朝 ProN.ttc"
+    else:
+        return "/usr/share/fonts/opentype/ipafont-mincho/ipam.ttf"
+
+
 def make_kanji_images():
     with open(os.path.join(PackageRoot, "kanji.txt")) as fp:
         chars = fp.read().split()
     for i, c in enumerate(chars):
         img = Image.new("L", (32, 32), color=255)
         draw = ImageDraw.Draw(img)
-        font = ImageFont.truetype("/System/Library/Fonts/ヒラギノ明朝 ProN.ttc", 30)
+        font = ImageFont.truetype(font_location(), 30)
         draw.text((1, 1), c, font=font)
         img.save(os.path.join(KANJI_IMG_PATH, f"{i}.png"))
