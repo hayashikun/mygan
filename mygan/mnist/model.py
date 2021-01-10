3c180585b30408a4ff33b53154374dafb3357e32
Ryosuke Hayashi <hayashi@hayashikun.com>
2021-01-10 21:40:16 +0900

    Revert

--- mygan/mnist/model.py
+++ mygan/mnist/model.py
@@ -4,29 +4,40 @@ from mygan.mnist import NOISE_DIM
 
 
 def make_generator():
-    return Sequential([
-        layers.Dense(7 * 7 * 256, use_bias=False, input_shape=(NOISE_DIM,)),
-        layers.BatchNormalization(),
-        layers.LeakyReLU(),
-        layers.Reshape((7, 7, 256)),
-        layers.Conv2DTranspose(128, kernel_size=5, strides=1, padding='same', use_bias=False),
-        layers.BatchNormalization(),
-        layers.LeakyReLU(),
-        layers.Conv2DTranspose(64, kernel_size=5, strides=2, padding='same', use_bias=False),
-        layers.BatchNormalization(),
-        layers.LeakyReLU(),
-        layers.Conv2DTranspose(1, kernel_size=5, strides=2, padding='same', use_bias=False, activation='tanh'),
-    ])
+    model = Sequential()
+    model.add(layers.Dense(7 * 7 * 256, use_bias=False, input_shape=(NOISE_DIM,)))
+    model.add(layers.BatchNormalization())
+    model.add(layers.LeakyReLU())
+    model.add(layers.Reshape((7, 7, 256)))
+    assert model.output_shape == (None, 7, 7, 256)
+
+    model.add(layers.Conv2DTranspose(128, kernel_size=5, strides=1, padding='same', use_bias=False))
+    assert model.output_shape == (None, 7, 7, 128)
+    model.add(layers.BatchNormalization())
+    model.add(layers.LeakyReLU())
+
+    model.add(layers.Conv2DTranspose(64, kernel_size=5, strides=2, padding='same', use_bias=False))
+    assert model.output_shape == (None, 14, 14, 64)
+    model.add(layers.BatchNormalization())
+    model.add(layers.LeakyReLU())
+
+    model.add(layers.Conv2DTranspose(1, kernel_size=5, strides=2, padding='same', use_bias=False, activation='tanh'))
+    assert model.output_shape == (None, 28, 28, 1)
+
+    return model
 
 
 def make_discriminator():
-    return Sequential([
-        layers.Conv2D(64, (5, 5), strides=(2, 2), padding='same', input_shape=(28, 28, 1)),
-        layers.LeakyReLU(),
-        layers.Dropout(0.3),
-        layers.Conv2D(128, (5, 5), strides=(2, 2), padding='same'),
-        layers.LeakyReLU(),
-        layers.Dropout(0.3),
-        layers.Flatten(),
-        layers.Dense(1)
-    ])
+    model = Sequential()
+    model.add(layers.Conv2D(64, (5, 5), strides=(2, 2), padding='same', input_shape=(28, 28, 1)))
+    model.add(layers.LeakyReLU())
+    model.add(layers.Dropout(0.3))
+
+    model.add(layers.Conv2D(128, (5, 5), strides=(2, 2), padding='same'))
+    model.add(layers.LeakyReLU())
+    model.add(layers.Dropout(0.3))
+
+    model.add(layers.Flatten())
+    model.add(layers.Dense(1))
+
+    return model
